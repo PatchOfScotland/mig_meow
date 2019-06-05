10863148c9412f96d84cab98379d66a0e80fb1ac
PatchOfScotland <d.marchant@ed-alumni.net>
2019-06-05 11:37:36 +0200

    setting up for pypi distribution

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mig_meow
-An event oriented scientific workflow manager for the MiG 
+
+MEOW is a Manager for Event Oriented Workflows.
+
+It is designed to interact with the MiG 
+(https://sourceforge.net/projects/migrid/) and provides methods for 
+workflows to be defined and manipulated within jupyter notebooks.
+
--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from distutils.core import setup
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(name='mig_meow',
+      version='0.1',
       author='David Marchant',
       author_email='d.marchant@ed-alumni.net',
+      description='MiG based manager for event oriented workflows',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/PatchOfScotland/mig_meow',
-      version='0.1',
-      py_modules=['mig_meow'])
+      packages=['mig_meow'],
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: GNU GENERAL PUBLIC LICENSE',
+            'Operating System :: OS Independent'
+      ])
