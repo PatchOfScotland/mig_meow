e6ea3c3d084823016649802089b76649bc1dfa56
PatchOfScotland <d.marchant@ed-alumni.net>
2019-11-28 12:57:28 +0100

    updated version to 0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 module_name = 'mig_meow'
 module_fullname = 'Managing Event-Oriented_workflows'
-module_version = '0.8'
+module_version = '0.9'
 
 setup(name=module_name,
       version=module_version,
