e3e9d8681f3c0566556b147cd03e7707792c6405
PatchOfScotland <d.marchant@ed-alumni.net>
2020-07-29 14:34:40 +0200

    reformated setup.py and versioning so only one update needed for core module information

--- mig_meow/info.py
+++ mig_meow/info.py
@@ -1,7 +1,4 @@
-
-module_name = 'mig_meow'
-module_fullname = 'Managing Event-Oriented_workflows'
-module_version = '0.13'
+import os
 
 
 def info():
@@ -11,10 +8,29 @@ def info():
     
     :return: (str) debug message.  
     """
-    message = 'ver: %s\n' \
+    msg = "No version data available"
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_path = os.path.join(
+        current_dir[:current_dir.rfind(os.path.sep)],
+        'version.py'
+    )
+
+    module_data = {}
+    try:
+        module_data = {}
+        with open(version_path) as f:
+            exec(f.read(), {}, module_data)
+    except:
+        pass
+
+    if module_data:
+        msg = 'ver: %s\n' \
               '%s has been imported correctly. \n' \
               '%s is a paradigm used for defining event based ' \
               'workflows. It is designed primarily to work with IDMC, a MiG ' \
               'implementation available at the University of Copenhagen. ' \
-              % (module_version, module_name, module_fullname)
-    print(message)
+              % (module_data['__version__'],
+                 module_data['__name__'],
+                 module_data['__fullname__'])
+    print(msg)
